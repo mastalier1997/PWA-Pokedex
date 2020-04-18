dcd96b4be3f418004fb08fd3bb9c11e1c81a2737
mahsum97 <mahsum.yildirim97@gmail.com>
2020-04-19 00:43:38 +0200

    Update Design.js/css

--- src/Detail.js
+++ src/Detail.js
@@ -1,25 +1,29 @@
 import React, {useState} from 'react';
 import './Detail.css';
-import {ClassDeclaration as pokemon} from "@babel/types";
 
 const queryString = window.location.search;
 console.log(queryString);
 const urlParams = new URLSearchParams(queryString);
+const id = urlParams.get('id');
+Detail(id)
 
-const Pokedex = require('pokeapi-js-wrapper');
-const P = new Pokedex.Pokedex();
-
-function Details(pokemon) {
-    const id = urlParams.get('id')
-
+function Detail(pokemon) {
     return (
-            <div className="Cell">
-                <div className="Cell_img">
-                    <img src={pokemon.sprites.front_default} alt="" />
+            <div className="Detail">
+                <div className="Detail_img">
+                    <p>TEST</p>
                 </div>
-                <div className="Cell_name">
+                <div className="Detail_name">
                     {pokemon.name}
                 </div>
             </div>
     );
 }
+
+async function getPoke(id) {
+    console.log(id);
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const json = await res.json();
+    console.log(json);
+}
+export default Detail;
