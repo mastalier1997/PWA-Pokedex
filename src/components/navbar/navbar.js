7585ef9da577ca095f524f4f6181593a0c091e1b
jan <jan.mastalier@gmail.com>
2020-05-06 22:19:49 +0200

    mobile style

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import './navbar.css';
 import {Button, Form, FormControl, Nav, NavDropdown} from "react-bootstrap";
+import Navbar from "react-bootstrap/lib/Navbar";
 
 
-function Navbar() {
+function Navbar1() {
     return (
-        <form className={"form-inline"}>
-            <label>Pokémon PWA</label>
-            <div className={"search-nav"}>
-                <input type={"text"} placeholder={"Search"}/>
-                <Button>Search</Button>
-            </div>
-        </form>
+        <Navbar>
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Form className={"form-inline"}>
+                <label>Pokémon PWA</label>
+                <div className={"search-nav"}>
+                    <FormControl type={"text"} placeholder={"Search"}/>
+                    <Button>Search</Button>
+                </div>
+            </Form>
+            </Navbar.Collapse>
+        </Navbar>
     );
 }
 
-export default Navbar;
+export default Navbar1;
 
